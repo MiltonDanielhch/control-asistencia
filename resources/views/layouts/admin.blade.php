42cd826c25672b2e9dcc63a0d6ae2e330c523fa2
milton <miltondanielhch617@gmail.com>
2024-05-18 16:45:10 -0400

    cargando datos y agregando datatables

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -17,6 +17,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <link rel="stylesheet" href="{{ url('plugins/fontawesome-free/css/all.min.css') }}">
     <!--  Icons Bootstrap  -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <!-- datatables -->
+    <link rel="stylesheet" href="{{ url('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ url('plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ url('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+
+    <!-- JQuery -->
+    <script src="{{ url('/plugins/jquery/jquery.js') }}"></script>
+
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ url('dist/css/adminlte.min.css') }}">
 </head>
@@ -169,8 +177,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="{{ url('/') }}" class="brand-link">
-                <img src="{{ url('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-                    style="opacity: .8">
+                <img src="{{ url('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+                    class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">SIS CONTROL</span>
             </a>
 
@@ -179,7 +187,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="{{ url('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                        <img src="{{ url('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
+                            alt="User Image">
                     </div>
                     <div class="info">
                         <a href="#" class="d-block">{{ Auth::user()->name }}</a>
@@ -276,6 +285,22 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <script src="{{ url('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <!-- AdminLTE App -->
     <script src="{{ url('dist/js/adminlte.min.js') }}"></script>
-</body>
 
+    <script src="{{ url('plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ url('plugins/jszip/jszip.min.js') }}"></script>
+    <script src="{{ url('plugins/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ url('plugins/pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ url('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+
+</body>
 </html>
+
+
+
--- resources/views/layouts/miembros/index.blade.php
+++ resources/views/layouts/miembros/index.blade.php
@@ -1,11 +1,68 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="content">
-        <h1>Listado de miembro</h1>
-        @foreach ($miembros as $miembro)
-            <h1>{{ $miembro->nombre_apellido }}</h1>
-            <h2>{{ $miembro->email }}</h2>
-        @endforeach
+<div class="content" style="margin-left: 10px">
+    <h1>Listado de miembro</h1>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card card-outline card-primary">
+                <div class="card-header">
+                    <h3 class="card-title"><b>Miembros Registrados</b></h3>
+                </div>
+
+
+
+                <div class="card-body" style="display: block;">
+
+                    <table id="example1" class="table table-bordered table-striped table-sm">
+                        <thead>
+                            <tr>
+                                <th>Nro</th>
+                                <th>Nombre y Apellido</th>
+                                <th>Teléfono</th>
+                                <th>Email</th>
+                                <th>Estado</th>
+                                <th>Agregado</th>
+                                <th>Accion</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @php
+                            $contador = 0;
+                            @endphp
+
+                            @foreach ($miembros as $miembro)
+                            <tr>
+                                <td>
+                                    @php
+                                    echo $contador = $contador + 1;
+                                    @endphp
+                                </td>
+                                <td>{{ $miembro->nombre_apellido }}</td>
+                                <td>{{ $miembro->telefono }}</td>
+                                <td>{{ $miembro->email }}</td>
+                                <td>{{ $miembro->estado }}</td>
+                                <td>{{ $miembro->fecha_ingreso }}</td>
+                                <td></td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    <script>
+                        $(function () {
+                          $("#example1").DataTable({
+                            "responsive": true, "lengthChange": true, "autoWidth": false,
+                            "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+                          }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+
+                        });
+                    </script>
+                </div>
+
+            </div>
+
+        </div>
     </div>
+</div>
 @endsection
